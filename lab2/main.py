0d4f24514b9b5f02f09667079044bf745e2ac477
Nik Peleshatyi <s2150635@frost.inf.ed.ac.uk>
2024-10-07 15:05:18 +0100

    xmlparser and index update. lab2 test collection script

--- lab2/main.py
+++ lab2/main.py
@@ -1,5 +1,20 @@
 import xml.etree.ElementTree as ET
+from utils.xmlparser import XmlDictConfig
+from utils.indexing import InvertedFrequencyIndex
 
-collection = "sample.xml"
-tree = ET.parse(f"data/collections/{collection}")
-print(tree)
+
+index = InvertedFrequencyIndex()
+
+def load_collection(collection_name="sample.xml"):
+    # collection = "sample.xml"
+    tree = ET.parse(f"data/collections/{collection_name}")
+    root = tree.getroot()
+    docs = [XmlDictConfig(child) for child in root]
+
+    for doc in docs:
+        doc_id = int(doc.get("DOCNO", None))
+        doc_text = doc.get("Text", "").strip()
+        if not doc_id or not doc_text:
+            print(f"Error, empty entry")
+            continue
+        index.add_document_to_index(doc_id, doc_text)
--- utils/indexing.py
+++ utils/indexing.py
@@ -1,24 +1,42 @@
 import re
-# `and:`
-# extracts 'and' before colon
-term_regex = re.compile(r"(.*?):")
-# `    4: 1,2,3`
-# splits into groups: (group_1: '4'), (group_2: '1,2,3')
-doc_positions_regex = re.compile("\t(.*?):\s(.*)")
+from utils.processing import tokenize_text, remove_stopwords, normalize
 
 
 class InvertedFrequencyIndex:
+    """
+    Inverted Frequency Index - digests documents and creates a map of 
+    processed tokens onto the relevant documents with positions
+    """
+    # `and:` - extracts 'and' before colon
+    term_regex = re.compile(r"(.*?):")
+    # `    4: 1,2,3` - splits into groups: (group_1: '4'), (group_2: '1,2,3')
+    doc_positions_regex = re.compile("\t(.*?):\s(.*)")
+    
     terms: dict
+    document_processing_pipeline: list
 
     def __init__(self):
         self.terms = {}
+        self.document_processing_pipeline = [tokenize_text, remove_stopwords, normalize]
     
-    def add_document_to_index(document):
-        return
+    def add_document_to_index(self, document_id, text):
+        tokens = text
+        for preprocessing_func in self.document_processing_pipeline:
+            tokens = preprocessing_func(tokens)
+        
+        for position, token in enumerate(tokens):
+            self.add_term_to_index(token, document_id, position+1)
     
     def add_term_to_index(self, term: str, document_id: int, position: int):
+        """
+        Takes a term, doc_id, position in the doc
+        and adds it to the index map
+        """
         if term in self.terms:
-            self.terms[term][document_id].append(position)
+            if document_id in self.terms[term]:
+                self.terms[term][document_id].append(position)
+            else:
+                self.terms[term][document_id] = [position]
         else:
             self.terms[term] = {
                 document_id: [position]
@@ -28,6 +46,14 @@ class InvertedFrequencyIndex:
         return
     
     def save(self, index_path):
+        """
+        Stores the index in the serializable format
+        
+        ```
+        token:
+            4: 1,2,3
+        ```
+        """
         with open(index_path, "w") as index_file:
             for term in self.terms:
                 doc_freqs = "\n".join(
@@ -39,6 +65,9 @@ class InvertedFrequencyIndex:
                 index_file.write(term_index_data)
     
     def load(self, index_path):
+        """
+        Loads the serialized index line by line (for efficiency)
+        """
         with open(index_path, "r") as index_file:
             term = None
             for index_line in index_file:
@@ -50,14 +79,14 @@ class InvertedFrequencyIndex:
                 # if the term is not set, we are starting to read the new entry
                 # and thus it needs to be set
                 if term == None:
-                    result = term_regex.search(index_line)
+                    result = self.term_regex.search(index_line)
                     term = result.group(1)
                     self.terms[term] = {}
                     continue
                 # otherwise, it is a document_id with positions
-                res = doc_positions_regex.search(index_line)
+                res = self.doc_positions_regex.search(index_line)
                 doc_id = int(res.group(1))
-                positions = tuple(map(int, res.group(2).split(",")))
+                positions = map(int, res.group(2).split(","))
                 self.terms[term][doc_id] = positions
 
 
