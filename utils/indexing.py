b4aab13e8f0df6838eac56cb817db36265774cba
Nik Peleshatyi <s2150635@schloss.inf.ed.ac.uk>
2024-10-17 16:25:34 +0100

    search update

--- utils/indexing.py
+++ utils/indexing.py
@@ -1,4 +1,5 @@
 import re
+from typing import Set
 from utils.processing import tokenize_text, remove_stopwords, normalize
 
 """
@@ -19,10 +20,12 @@ class InvertedFrequencyIndex:
     # `    4: 1,2,3` - splits into groups: (group_1: '4'), (group_2: '1,2,3')
     doc_positions_regex = re.compile("\t(.*?):\s(.*)")
     
+    doc_ids: set
     terms: dict
     text_processing_pipeline: list
 
     def __init__(self):
+        self.doc_ids = set()
         self.terms = {}
         self.text_processing_pipeline = [tokenize_text, remove_stopwords, normalize]
     
@@ -33,6 +36,7 @@ class InvertedFrequencyIndex:
         
         for position, token in enumerate(tokens):
             self.add_term_to_index(token, document_id, position+1)
+        self.doc_ids.add(document_id)
     
     def add_term_to_index(self, term: str, document_id: int, position: int):
         """
@@ -48,17 +52,108 @@ class InvertedFrequencyIndex:
             self.terms[term] = {
                 document_id: [position]
             }
+    
+    def check_term_in_document(self, doc_id, term) -> bool:
+        """
+        check if the term is present in a given document
+        """
+        return True if self.terms[term][doc_id] else False
+    
+    def phrase_search(self, query, strict=False) -> Set[int]:
+        # if strict (exact match), enfore set intersection, else union over pairs
+        set_operator = set.intersection if strict else set.union
+        for func in self.text_processing_pipeline:
+            query_tokens = func(query)
+        print("Query tokens", query_tokens)
+        docs = None
+        for pair in zip(query_tokens, query_tokens[1:]):
+            result = self.proximity_search(*pair, n=1)
+            # if not the first pass, apply operator
+            if docs:
+                docs = set_operator(docs, result)
+            else:
+                docs = result
+        return docs
+    
+    def check_terms_close_in_document(self, doc_id, term1, term2, n) -> bool:
+        # term occurrences in a document
+        i1 = self.terms[term1][doc_id]
+        i2 = self.terms[term2][doc_id]
+        # list pointers
+        i = 0
+        j = 0
+        while i < len(i1) and j < len(i2):
+            # while i1 is before i2 and while the diff > n, move to next i1
+            while i1[i] < i2[j] and i2[j] - i1[i] > n:
+                i += 1
+            if i2[j] - i1[i] <= n:
+                return True
+            j += 1
+        return False
 
-    def proximity_search(self):
-        return
+    def proximity_search(self, term1, term2, n) -> Set[int]:
+        """
+        Performs proximity search over the index
+        Returns relevant documents, where both term1 and term2
+        are present, and the distance between them is <= n
+        """
+        print(term1, term2)
+        # position lists
+        d1 = self.terms[term1]
+        d2 = self.terms[term2]
+        relevant_doc_ids = set()
+        for doc_id in d1.keys():
+            # if doc_id not common, continue
+            if doc_id not in d2:
+                continue
+            if self.check_terms_close_in_document(doc_id, term1, term2, n):
+                relevant_doc_ids.add(doc_id)
+        return relevant_doc_ids
+    
+    def evaluate_expression(self, exp):
+        negated = False
+        docs = set()
+        if "NOT" in exp:
+            negated = True
+            exp = re.findall(r"NOT\s?", exp)[0]
+        # #int(str,str)
+        proximity_regex = r"#(\d+)\((\w+),\s?(\w+)\)"
+        # "str"
+        phrase_regex = r"\"(.*)\""
+        if re.search(proximity_regex, exp):
+            n, t1, t2 = re.findall(proximity_regex, exp)[0]
+            docs = self.proximity_search(t1, t2, n)
+        # phrase search
+        elif re.search(phrase_regex, exp):
+            exact_query = re.findall(phrase_regex, exp)[0]
+            docs = self.phrase_search(exact_query, strict=True)
+        # regular search
+        else:
+            docs = self.phrase_search(exp)
+        
+        if negated:
+            return self.doc_ids.difference(docs)
+        return docs
 
     def search(self, query):
-        query = "income AND NOT taxes"
         # Boolean search (AND, OR, NOT)
         # phrase search ("exact match")
         # proximity search pos(term2) – pos(term1) < |w| -> #5(term1,term2)
         # ranked IR search based on TFIDF
-        return
+        operator = None
+        # split on AND/OR, if present, otherwise just find
+        # Left AND/OR Right
+        if "AND" in query:
+            operator = set.intersection
+            query = query.split("AND")
+
+        elif "OR" in query:
+            operator = set.union
+            query = query.split("OR")
+        else:
+            return self.evaluate_expression(query)
+        print(query)
+        return operator(map(self.evaluate_expression, query))
     
     def save_to_file(self, index_path):
         """
@@ -102,9 +197,14 @@ class InvertedFrequencyIndex:
                 res = self.doc_positions_regex.search(index_line)
                 doc_id = int(res.group(1))
                 positions = map(int, res.group(2).split(","))
+                self.doc_ids.add(doc_id)
                 self.terms[term][doc_id] = positions
 
 
 if __name__ == "__main__":
     index = InvertedFrequencyIndex()
     index.load_from_file("data/collections/collection.index.txt")
+    query = input("Q:")
+    while True:
+        print(index.search(query))
+        query = input("Q:")
